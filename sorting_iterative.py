09dd58cc61464ee51a3a2b95f343f6c82705ea97
Jetpuffed <brandon@jetpuffed.org>
2020-10-22 20:22:30 -0700

    add: is_sorted() and bubble_sort() use key parameter to enable advanced sorting

--- sorting_iterative.py
+++ sorting_iterative.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 
 
-def is_sorted(items, reverse=False):
+def is_sorted(items, key=None, reverse=False):
     """Return a boolean indicating whether given items are in sorted order.
     Time Complexity: O(n) - It iterates through the entire length of the array.
     Space Complexity: O(n) - Memory usage grows in relation to the length of the array."""
     for i, j in enumerate(items[1:]):
+        if key is not None:
+            if key(j) < key(items[i]) and reverse is False or key(j) > key(items[i]) and reverse is True:
+                return False
         if j < items[i] and reverse is False or j > items[i] and reverse is True:
             return False
     return True
 
 
-def bubble_sort(items, reverse=False):
+def bubble_sort(items, key=None, reverse=False):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
     Time Complexity: O(n^2) - It iterates exponentially through the length of the array.
     Space Complexity: O(1) - Memory usage does not grow because of in-place sorting."""
     while True:
         for i, _ in enumerate(items[1:]):
+            if key is not None:
+                if key(items[i + 1]) < key(items[i]) and reverse is False or key(items[i + 1]) > key(items[i]) and reverse is True:
+                    items[i + 1], items[i] = items[i], items[i + 1]
             if items[i + 1] < items[i] and reverse is False or items[i + 1] > items[i] and reverse is True:
                 items[i + 1], items[i] = items[i], items[i + 1]
-        if is_sorted(items, order):
+        if is_sorted(items, key=key, reverse=reverse):
             break
     return items
 
@@ -37,7 +43,7 @@ def selection_sort(items, reverse=False):
                 min = i + 1
             if min != i:
                 items[i], items[min] = items[min], items[i]
-        if is_sorted(items, order):
+        if is_sorted(items, reverse):
             break
     return items
 
@@ -55,6 +61,6 @@ def insertion_sort(items, reverse=False):
                 items[j + 1] = items[j]
                 j -= 1
             items[j + 1] = k
-        if is_sorted(items, order):
+        if is_sorted(items, reverse):
             break
     return items
